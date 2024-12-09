d9496a494dd69b5aa131ce876a38ec0af9e79e8d
Edidiong Udoh <gisthive@gmail.com>
2024-12-09 09:29:38 +0100

    fixes

--- app/new-password/page.tsx
+++ app/new-password/page.tsx
@@ -2,7 +2,7 @@
 // app/new-password/page.tsx
 "use client"; // Indicate that this is a client component
 
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation"; // Import useRouter and useSearchParams
 import AltHeader from "@/components/alt-header";
 import { toast } from "react-hot-toast"; // Import toast
@@ -47,97 +47,106 @@ const NewPasswordPage = () => {
   const passwordsMatch = newPassword === confirmPassword;
 
   return (
-    <div className="flex flex-col min-h-screen bg-white">
-      {/* Header */}
-      <AltHeader />
+    <Suspense>
+      <div className="flex flex-col min-h-screen bg-white">
+        {/* Header */}
+        <AltHeader />
 
-      {/* Main Content */}
-      <main className="flex-grow flex items-center justify-center p-4">
-        <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full">
-          <h2 className="text-2xl font-bold text-left mb-2">New Password</h2>
-          <p className="text-sm text-gray-600 text-left mb-4">
-            Create new password
-          </p>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                New password *
-              </label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Confirm password *
-              </label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              disabled={!passwordsMatch || isLoading} // Disable button if passwords do not match or loading
-              className={`w-full text-white font-bold py-2 rounded-md transition duration-200 ${
-                passwordsMatch && !isLoading
-                  ? "bg-blue-600 hover:bg-blue-700"
-                  : "bg-gray-400 cursor-not-allowed"
-              }`} // Change class based on password match and loading state
-            >
-              {isLoading ? (
-                <span className="flex items-center justify-center">
-                  <svg
-                    className="animate-spin h-5 w-5 mr-3 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    />
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8v8H4z"
-                    />
-                  </svg>
-                  Loading...
-                </span>
-              ) : (
-                "Reset password"
-              )}
-            </button>
-          </form>
-        </div>
-      </main>
-
-      {/* Page Selector */}
-      <footer className="flex justify-center items-center py-4">
-        <div className="flex items-center">
-          <div className="flex ml-2">
-            <div className="w-3 h-3 bg-gray-300 rounded-full mx-1"></div>
-            <div className="w-3 h-3 bg-gray-300 rounded-full mx-1"></div>
-            <div className="w-8 h-3 bg-blue-600 rounded-full mx-1"></div>
+        {/* Main Content */}
+        <main className="flex-grow flex items-center justify-center p-4">
+          <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full">
+            <h2 className="text-2xl font-bold text-left mb-2">New Password</h2>
+            <p className="text-sm text-gray-600 text-left mb-4">
+              Create new password
+            </p>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  New password *
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">
+                  Confirm password *
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                disabled={!passwordsMatch || isLoading} // Disable button if passwords do not match or loading
+                className={`w-full text-white font-bold py-2 rounded-md transition duration-200 ${
+                  passwordsMatch && !isLoading
+                    ? "bg-blue-600 hover:bg-blue-700"
+                    : "bg-gray-400 cursor-not-allowed"
+                }`} // Change class based on password match and loading state
+              >
+                {isLoading ? (
+                  <span className="flex items-center justify-center">
+                    <svg
+                      className="animate-spin h-5 w-5 mr-3 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      />
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8v8H4z"
+                      />
+                    </svg>
+                    Loading...
+                  </span>
+                ) : (
+                  "Reset password"
+                )}
+              </button>
+            </form>
           </div>
-        </div>
-      </footer>
-    </div>
+        </main>
+
+        {/* Page Selector */}
+        <footer className="flex justify-center items-center py-4">
+          <div className="flex items-center">
+            <div className="flex ml-2">
+              <div className="w-3 h-3 bg-gray-300 rounded-full mx-1"></div>
+              <div className="w-3 h-3 bg-gray-300 rounded-full mx-1"></div>
+              <div className="w-8 h-3 bg-blue-600 rounded-full mx-1"></div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </Suspense>
   );
 };
 
-export default NewPasswordPage;
+const WrappedNewPasswordPage = () => {
+  return (
+    <Suspense>
+      <NewPasswordPage />
+    </Suspense>
+  );
+};
+export default WrappedNewPasswordPage;
--- app/password-verification/page.tsx
+++ app/password-verification/page.tsx
@@ -1,7 +1,7 @@
 // app/verification/page.tsx
 "use client";
 
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import AltHeader from "@/components/alt-header";
@@ -11,7 +11,7 @@ import { postRequest } from "@/helpers/api"; // Import postRequest helper
 const VerificationPage = () => {
   const [code, setCode] = useState(["", "", "", "", "", ""]); // State to hold the 6-digit code
   const router = useRouter();
-  const searchParams = useSearchParams(); // Use useSearchParams to get query params
+  const searchParams = useSearchParams();
   const email = searchParams.get("email"); // Get email from search params
   const [isLoading, setIsLoading] = useState(false); // State to manage loading
 
@@ -76,66 +76,76 @@ const VerificationPage = () => {
   const isCodeComplete = code.every((digit) => digit !== "");
 
   return (
-    <div className="flex flex-col min-h-screen bg-white">
-      {/* Header */}
-      <AltHeader />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex flex-col min-h-screen bg-white">
+        {/* Header */}
+        <AltHeader />
 
-      {/* Main Content */}
-      <main className="flex-grow flex items-center justify-center p-4">
-        <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full">
-          <h2 className="text-2xl font-bold text-left mb-2">Verification</h2>
-          <p className="text-sm text-gray-600 text-left mb-4">
-            Enter the 6-digit code sent to your email address
-          </p>
-          <div className="flex justify-center mb-4">
-            {code.map((digit, index) => (
-              <input
-                key={index}
-                type="text"
-                value={digit}
-                onChange={(e) => handleChange(index, e.target.value)}
-                className="w-12 h-12 border border-blue-600 rounded-md text-center text-xl mx-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                maxLength={1}
-                data-index={index}
-              />
-            ))}
-          </div>
-          <button
-            type="button"
-            onClick={handleContinue}
-            disabled={!isCodeComplete || isLoading} // Disable button if code is not complete or loading
-            className={`w-full text-white font-bold py-2 rounded-md transition duration-200 ${
-              isCodeComplete && !isLoading
-                ? "bg-blue-600 hover:bg-blue-700"
-                : "bg-gray-400 cursor-not-allowed"
-            }`} // Change class based on code completion and loading state
-          >
-            {isLoading ? "Loading..." : "Continue"}
-          </button>
-          <div className="text-center mt-4">
-            <Link
-              href="/forgot-password"
-              onClick={() => router.back()}
-              className="text-blue-500 hover:text-blue-700"
+        {/* Main Content */}
+        <main className="flex-grow flex items-center justify-center p-4">
+          <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full">
+            <h2 className="text-2xl font-bold text-left mb-2">Verification</h2>
+            <p className="text-sm text-gray-600 text-left mb-4">
+              Enter the 6-digit code sent to your email address
+            </p>
+            <div className="flex justify-center mb-4">
+              {code.map((digit, index) => (
+                <input
+                  key={index}
+                  type="text"
+                  value={digit}
+                  onChange={(e) => handleChange(index, e.target.value)}
+                  className="w-12 h-12 border border-blue-600 rounded-md text-center text-xl mx-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  maxLength={1}
+                  data-index={index}
+                />
+              ))}
+            </div>
+            <button
+              type="button"
+              onClick={handleContinue}
+              disabled={!isCodeComplete || isLoading} // Disable button if code is not complete or loading
+              className={`w-full text-white font-bold py-2 rounded-md transition duration-200 ${
+                isCodeComplete && !isLoading
+                  ? "bg-blue-600 hover:bg-blue-700"
+                  : "bg-gray-400 cursor-not-allowed"
+              }`} // Change class based on code completion and loading state
             >
-              Resend code
-            </Link>
+              {isLoading ? "Loading..." : "Continue"}
+            </button>
+            <div className="text-center mt-4">
+              <Link
+                href="/forgot-password"
+                onClick={() => router.back()}
+                className="text-blue-500 hover:text-blue-700"
+              >
+                Resend code
+              </Link>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
 
-      {/* Page Selector */}
-      <footer className="flex justify-center items-center py-4">
-        <div className="flex items-center">
-          <div className="flex ml-2">
-            <div className="w-3 h-3 bg-gray-300 rounded-full mx-1"></div>
-            <div className="w-8 h-3 bg-blue-600 rounded-full mx-1"></div>
-            <div className="w-3 h-3 bg-gray-300 rounded-full mx-1"></div>
+        {/* Page Selector */}
+        <footer className="flex justify-center items-center py-4">
+          <div className="flex items-center">
+            <div className="flex ml-2">
+              <div className="w-3 h-3 bg-gray-300 rounded-full mx-1"></div>
+              <div className="w-8 h-3 bg-blue-600 rounded-full mx-1"></div>
+              <div className="w-3 h-3 bg-gray-300 rounded-full mx-1"></div>
+            </div>
           </div>
-        </div>
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </Suspense>
   );
 };
 
-export default VerificationPage;
+function WrappedVerificationPage() {
+  return (
+    <Suspense>
+      <VerificationPage />
+    </Suspense>
+  );
+}
+
+export default WrappedVerificationPage;
